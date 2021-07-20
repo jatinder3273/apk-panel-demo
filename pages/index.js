0c7efb520fda97d3740e7b15d023bbd1b755c30c
Jatinder <jatinder.kumar@softradix.com>
2021-07-20 17:00:40 +0530

    hello

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,5 @@ import React, { Component } from "react";
 import Router from "next/router";
 
 export default function Index() {
-  React.useEffect(() => {
-    Router.push("/admin/dashboard");
-  });
-
-  return <div />;
+  return <div>Index</div>;
 }
